36d1a2573e0b393e268796e0337084021642f391
Paulo Eduardo da Silva Rosa <Paulo-Rosa@PROCERGS.REDERS>
2017-03-10 15:54:53 -0300

    CRUD Users

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('user/list');
+});
+
+/* GET users form new user. */
+router.get('/new', function(req, res, next) {
+  res.render('user/new');
 });
 
 module.exports = router;
